e4f789bf03f952c009fe9451bd6f661829b72407
JGadi <johnpgadi@gmail.com>
2019-04-11 11:30:10 -0700

    completed san diego top spots

---
+++ README.md
@@ -0,0 +1,5 @@
+Hello, welcome to San Diego Top Spots.
+
+This will show a list of top spots in San Diego with links to each spot on google maps.
+
+Load San Diego Top Spots by typing "static ." in the command line and going to http://127.0.0.1:8080/ in your browser. Thanks for visiting!
--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
 // write your code here
-
+$.getJSON( "data.json", function( data ) {
+    var spots = [];
+  $.each( data, function( key, val ) {
+    spots += '<tr>';
+    spots += '<td>'+ val.name+ '</td>';
+    spots += '<td>'+ val.description +'</td>';
+    spots += '<td nowrap="nowrap">'+ "<a href="+"https://www.google.com/maps?q="+val.location+'><button>Open in Google Maps</button></a></td>';
+    spots += '</tr>';
+  })
+  $('thead').append(spots);
 });
+});
+
